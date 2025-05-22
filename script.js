3c5283509e57929b084f7e3fd9ac5991e6d81eb6
trevisannatalia <n.ntrevisan@gmail.com>
2025-05-22 13:59:01 +0100

    fixFormTel

--- script.js
+++ script.js
@@ -133,3 +133,13 @@ document.getElementById('formContato').addEventListener('submit', async function
         erroForm.style.display = 'block';
     }
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+  var input = document.getElementById('whatsapp');
+  if (input) {
+    input.addEventListener('input', function(e) {
+      var x = e.target.value.replace(/\D/g, '').match(/(\d{0,2})(\d{0,5})(\d{0,4})/);
+      e.target.value = !x[2] ? x[1] : '(' + x[1] + ') ' + x[2] + (x[3] ? '-' + x[3] : '');
+    });
+  }
+});
